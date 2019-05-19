ac44da6c1a8b0989469787ee2b116f6f3c127f94
Michael Ho <tsungwei50521@hotmail.com>
2019-05-18 23:09:11 -0400

    Version 0.0.1 beta

---
+++ MaterialDesignWidgets/MaterialDesignWidgets/MaterialDesignWidgets.h
@@ -0,0 +1,19 @@
+//
+//  MaterialDesignWidgets.h
+//  MaterialDesignWidgets
+//
+//  Created by Ho, Tsung Wei on 5/16/19.
+//  Copyright © 2019 Michael Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialDesignWidgets.
+FOUNDATION_EXPORT double MaterialDesignWidgetsVersionNumber;
+
+//! Project version string for MaterialDesignWidgets.
+FOUNDATION_EXPORT const unsigned char MaterialDesignWidgetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialDesignWidgets/PublicHeader.h>
+
+
